ccde132570f9395be0cca5e274b1bb7481572178
IamEg <iameg@iameg.com>
2022-09-11 11:13:46 +0300

    Grid added

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import useWordle from "../hooks/useWordle";
+import Grid from "./Grid";
 
 export default function Wordle({ solution }) {
   const { currentGuess, handleKeyup ,guesses, isCorrect, turn} = useWordle(solution);
@@ -19,6 +20,7 @@ export default function Wordle({ solution }) {
     <div>
       <h1>currentGuess is : {currentGuess}</h1>
       <h2> Solution is : {solution} </h2>
+      <Grid currentGuess={currentGuess} guesses = {guesses} turn = {turn}/>
     </div>
   );
 }
