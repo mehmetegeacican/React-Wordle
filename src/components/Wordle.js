f585e5ea1f84c8960284c40d251dc8f172dc2aa2
IamEg <iameg@iameg.com>
2022-09-12 19:32:45 +0300

    Keypad implementation

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import useWordle from "../hooks/useWordle";
 import Grid from "./Grid";
+import Keypad from "./Keypad";
 
 export default function Wordle({ solution }) {
   const { currentGuess, handleKeyup ,guesses, isCorrect, turn} = useWordle(solution);
@@ -21,6 +22,7 @@ export default function Wordle({ solution }) {
       {/*<h1>currentGuess is : {currentGuess}</h1>*/}
       <h2> Solution is : {solution} </h2>
       <Grid currentGuess={currentGuess} guesses = {guesses} turn = {turn}/>
+      <Keypad/>
     </div>
   );
 }
